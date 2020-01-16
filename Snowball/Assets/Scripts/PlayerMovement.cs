6a46b4e8851cfb3d2ee48c9626de6e0cc5c656b9
Joakim <themanzed@gmail.com>
2020-01-16 23:42:05 +0100

    Added player movement and camera control

---
+++ Snowball/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float sprintMod = 1.5f;
+    public float gravity = -9.82f;
+
+    private CharacterController _charCont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charCont = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            movement *= sprintMod;
+        }
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charCont.Move(movement);
+    }
+}
